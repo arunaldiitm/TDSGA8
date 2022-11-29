5844d0bb7cb8bc0d866c36516e665f3756ab411e
arunaldiitm <119434373+arunaldiitm@users.noreply.github.com>
2022-11-29 21:51:20 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.header('enter two mumber')
-
-def user_input():
-    first_no = st.number_input("first_no", min_value = 0.0, max_value = 100000000000.0)
-    second_no = st.number_input("second_no", min_value = 0.0, max_value = 100000000000.0)
+first_no = st.number_input("first_no", min_value = 0.0, max_value = 100000000000.0)
+second_no = st.number_input("second_no", min_value = 0.0, max_value = 100000000000.0)
+sums = first_no + second_no
+st.subheader('sum of two numbers are')
+st.write(sums)
